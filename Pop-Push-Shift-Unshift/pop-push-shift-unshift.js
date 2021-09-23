c17b09eeaaf0fe46ab136c2c2d516a24aeed911a
Nasir Uddin Khan <uddin6058@gmail.com>
2021-09-23 15:24:49 +0600

    pop push shift unshift

---
+++ Pop-Push-Shift-Unshift/pop-push-shift-unshift.js
@@ -0,0 +1,22 @@
+var arr = ['Nasir', ' Sajjad', ' Maruf', 1084, true];
+console.log('Array : ' + arr);
+
+// Pop will delete element from last
+console.log('# pop() will delete element from last');
+arr.pop();
+console.log(arr);
+
+// Push will add element from last
+console.log('# push() will add element from last');
+arr.push(1888);
+console.log(arr);
+
+// Unshift will add element form first
+console.log('# unshift() will add element form first');
+arr.unshift(820074);
+console.log(arr);
+
+// Shift will delete element from first
+console.log('# shift() will delete element from first');
+arr.shift();
+console.log(arr);
