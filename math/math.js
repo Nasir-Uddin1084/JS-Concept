c92af0f64e48da221ddcd036de43f23e5f135b55
Nasir Uddin Khan <uddin6058@gmail.com>
2021-09-27 14:45:23 +0600

    math uses

---
+++ math/math.js
@@ -0,0 +1,57 @@
+console.log(Math.E);
+console.log(Math.PI);
+
+let n = 3.1415;
+let m = 2.49;
+let o = 2.51;
+// absolute number
+console.log('<<<<< absolute number >>>>>');
+console.log(Math.abs(n));
+console.log(Math.abs(m));
+console.log(Math.abs(o));
+
+// floor
+console.log('<<<<< floor >>>>>');
+console.log(Math.floor(n));
+console.log(Math.floor(m));
+console.log(Math.floor(o));
+
+// ceil
+console.log('<<<<< ceil >>>>>');
+console.log(Math.ceil(n));
+console.log(Math.ceil(m));
+console.log(Math.ceil(o));
+
+// round number
+console.log('<<<<< round number >>>>>');
+console.log(Math.round(n));
+console.log(Math.round(m));
+console.log(Math.round(o));
+
+// max
+console.log('<<<<< max >>>>>');
+
+console.log(Math.max(301, 301.01, 20, 90, 48.3, 80.32));
+
+// min
+console.log('<<<<< min >>>>>');
+console.log(Math.min(30, 120, 70, 48.3, 80.32));
+
+// power, square root
+console.log('<<<<< power >>>>>');
+
+// power
+console.log(Math.pow(3, 3));
+
+console.log('<<<<< square root >>>>>');
+// square root
+console.log(Math.sqrt(144));
+
+// random number
+console.log('<<<<< random numbers >>>>>');
+console.log(Math.random());
+console.log(Math.random() * 5);
+console.log(Math.round(Math.random() * 4));
+console.log(Math.abs(Math.random() * 6));
+console.log(Math.floor(Math.random() * 7));
+console.log(Math.ceil(Math.random() * 3));
