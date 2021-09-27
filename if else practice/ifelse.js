f77e9a9da93e598ea0c9622ebdde58f0fb71d4dd
Nasir Uddin Khan <uddin6058@gmail.com>
2021-09-28 01:56:05 +0600

    taking age from user

---
+++ if else practice/ifelse.js
@@ -0,0 +1,10 @@
+let age = prompt('Enter your age');
+if (age <= 30) {
+	document.getElementById('if-else').innerHTML =
+		'It is high time to work for JANNAH!';
+	console.log('It is high time to work for JANNAH!');
+} else {
+	document.getElementById('if-else').innerHTML =
+		'you will regrate in your times!';
+	console.log('you will regrate in your old times!');
+}
