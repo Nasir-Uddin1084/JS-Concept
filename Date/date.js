e2cc6a7ddbb7efb4abb2e0c55c1c188ccca5eeea
Nasir Uddin Khan <uddin6058@gmail.com>
2021-09-27 10:03:05 +0600

    date js

---
+++ Date/date.js
@@ -0,0 +1,25 @@
+let date = new Date();
+// date
+console.log(date);
+// 1st day of the week is Monday! (Western rules)
+console.log(date.getDay());
+// toString
+console.log(date.toString());
+// toLOcalString
+console.log(date.toLocaleString());
+// toTimeString
+console.log(date.toTimeString());
+// toLocaleTimeString
+console.log(date.toLocaleTimeString());
+// getFullYear
+console.log('Year: ' + date.getFullYear());
+// getMonth
+console.log('Month: ' + date.getMonth());
+// getDate
+console.log('Date: ' + date.getDate());
+// getHours
+console.log('Hours: ' + date.getHours());
+// getMinutes
+console.log('Minutes: ' + date.getMinutes());
+// getSeconds
+console.log('Seconds: ' + date.getSeconds());
